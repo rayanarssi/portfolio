7a7837b82d716265d95e70d5c807dda62cb679c6
rayanarssi <145460408+rayanarssi@users.noreply.github.com>
2025-09-23 11:00:52 +0200

    4th try

--- my-portfolio/src/components/Contact.jsx
+++ my-portfolio/src/components/Contact.jsx
@@ -27,67 +27,89 @@ const Contact = () => {
 
 	return (
 		<section id="contact" className="contact-section">
-			<div className="contact-info">
-				<h3>Let's work together</h3>
-				<p>
-					I'm currently looking for a stage opportunity where I can apply and
-					grow my skills in web development. I'm always excited about new
-					opportunities and projects, so whether you have an internship position
-					available or just want to chat about web development, feel free to
-					reach out.
-				</p>
-				<div className="contact-details">
-					<div className="detail-item">
-						<img src="/assets/email-icon.png" alt="Email Icon" />
-						<p>
-							Email
-							<br />
-							rayan.arssi@student.ehb.be
-						</p>
-					</div>
-					<div className="detail-item">
-						<img src="/assets/phone-icon.png" alt="Phone Icon" />
-						<p>
-							Phone
-							<br />
-							+32 465 56 98 96
-						</p>
-					</div>
-					<div className="detail-item">
-						<img src="/assets/location-icon.png" alt="Location Icon" />
-						<p>
-							Location
-							<br />
-							Belgium
-						</p>
+			<h2 className="contact-title">Get In Touch</h2>
+			<div className="contact-container">
+				<div className="contact-info">
+					<h3>Let's work together</h3>
+					<p>
+						I'm currently looking for a stage opportunity where I can apply and
+						grow my skills in web development. I'm always excited about new
+						opportunities and projects, so whether you have an internship
+						position available or just want to chat about web development, feel
+						free to reach out.
+					</p>
+					<div className="contact-details">
+						<div className="detail-item">
+							<img src="src/assets/Email_contact.svg" alt="Email Icon" />
+							<p>
+								<span className="contact_title">Email</span>
+								<br />
+								rayan.arssi@student.ehb.be
+							</p>
+						</div>
+						<div className="detail-item">
+							<img src="src/assets/Phone.svg" alt="Phone Icon" />
+							<p>
+								<span className="contact_title">Phone</span>
+								<br />
+								+32 465 56 98 96
+							</p>
+						</div>
+						<div className="detail-item">
+							<img src="src/assets/Location.svg" alt="Location Icon" />
+							<p>
+								<span className="contact_title">Location</span>
+								<br />
+								Belgium
+							</p>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div className="contact-form-container">
-				<h3>Send me a message</h3>
-				<form ref={form} onSubmit={sendEmail}>
-					<div className="form-group">
-						<label>First Name</label>
-						<input type="text" name="first_name" required />
-					</div>
-					<div className="form-group">
-						<label>Last Name</label>
-						<input type="text" name="last_name" required />
-					</div>
-					<div className="form-group full-width">
-						<label>Email Address</label>
-						<input type="email" name="user_email" required />
-					</div>
-					<div className="form-group full-width">
-						<label>Subject</label>
-						<input type="text" name="subject" required />
-					</div>
-					<div className="form-group full-width">
-						<label>Your message...</label>
-						<textarea name="message" required></textarea>
-					</div>
-					<button type="submit">Send Message</button>
-				</form>
+
+				<div className="contact-form-container">
+					<h3>Send me a message</h3>
+					<form ref={form} onSubmit={sendEmail}>
+						<div className="form-group">
+							<input
+								type="text"
+								name="first_name"
+								required
+								placeholder="First Name"
+							/>
+							<input
+								type="text"
+								name="last_name"
+								required
+								placeholder="Last Name"
+							/>
+						</div>
+
+						<div className="form-group full-width">
+							<input
+								type="email"
+								name="user_email"
+								required
+								placeholder="Email Address"
+							/>
+						</div>
+						<div className="form-group full-width">
+							<input
+								type="text"
+								name="subject"
+								required
+								placeholder="Subject"
+							/>
+						</div>
+						<div className="form-group full-width">
+							<textarea
+								name="message"
+								required
+								placeholder="Your message..."
+							></textarea>
+						</div>
+						<button type="submit">Send Message</button>
+					</form>
+				</div>
 			</div>
 		</section>
 	);
